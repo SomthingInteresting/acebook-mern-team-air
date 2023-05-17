0b926a31fad9a0ee8210e12292239f15dbe17c37
Arya Sunildath <aryadath@gmail.com>
2023-05-17 15:37:08 +0100

    installing dependencies

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -5,4 +5,9 @@ describe("Post", () => {
     cy.mount(<Post post={{_id: 1, message: "Hello, world"}} />);
     cy.get('[data-cy="post"]').should('contain.text', "Hello, world")
   })
+
+  it('posts message with fullname', () => {
+    cy.mount(<Post post={{id: 1, firstname:'Jack', lastname: 'test', message: 'Hello,world'}} />);
+    cy.get('[data-cy="post"]').should('contain.text', 'Jack test : Hello,world')
+  })
 })
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Post = ({post}) => {
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <article data-cy="post" key={ post._id }>
+      <div>
+        {post.firstname} {post.lastname}
+      </div>
+      <div>
+        { post.message }
+      </div>
+    </article>
   )
 }
 
